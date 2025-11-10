b2a23d1050495566fbc57f90efbef298bd5c1f96
Roman Ohiienko <ohiienko.r@gmail.com>
2025-11-10 21:46:49 +0200

    feat: complete useBackButton hook, update balance bg

--- src/hooks/useBackButton.tsx
+++ src/hooks/useBackButton.tsx
@@ -1,11 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
+import { useNavigate } from "react-router";
 import { backButton } from "@tma.js/sdk-react";
 
-export default function useBackButton() {
+interface UseBackButtonProps {
+  customCallback?: () => void;
+}
+
+export default function useBackButton({
+  customCallback,
+}: UseBackButtonProps = {}) {
+  const navigate = useNavigate();
+
+  const handleGoBack = useCallback(() => navigate(-1), [navigate]);
+
   useEffect(() => {
     backButton.show();
     return () => {
       backButton.hide();
     };
   }, []);
+
+  useEffect(() => {
+    if (customCallback) {
+      backButton.onClick(customCallback);
+    } else {
+      backButton.onClick(handleGoBack);
+    }
+
+    return () => {
+      if (customCallback) {
+        backButton.offClick(customCallback);
+      } else {
+        backButton.offClick(handleGoBack);
+      }
+    };
+  }, [customCallback, handleGoBack]);
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,12 +10,12 @@ export default function HomePage() {
   return (
     <main className="flex flex-col gap-6 p-2 size-full">
       <header className="flex justify-between items-center gap-2">
-        <h1 className="font-semibold">
+        <h1 className="font-semibold text-lg">
           Hi, {lp?.tgWebAppData?.user?.first_name ?? "Dear User"}👋
         </h1>
 
         <span className="bg-linear-to-r from-[#EA850F] to-[#FFC700] p-0.5 rounded-2xl size-fit">
-          <p className="flex items-center gap-1 bg-primary-purple px-2 py-0.5 rounded-full font-medium text-sm">
+          <p className="flex items-center gap-1 bg-[#06080d] px-2 py-0.5 rounded-full font-medium text-sm">
             <MoonCoinIcon className="size-4 text-white" />
             100
           </p>
